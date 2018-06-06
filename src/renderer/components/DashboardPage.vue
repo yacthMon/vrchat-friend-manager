d10f82cc17be1407f4693b1db8a625f52fd65899
yacth_Mon <yacthMon@users.noreply.github.com>
2018-06-06 17:24:15 +0700

    Add animation for friend list

--- src/renderer/components/DashboardPage.vue
+++ src/renderer/components/DashboardPage.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="wrapper" style="padding-right: 0px;">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <div class="container">
       <div class="row">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
         <div class="col-4">
           <span class="title">
             <h2>Profile</h2>
@@ -19,20 +19,22 @@
         <div class="col justify-content-end">
           {{getOnlineFriend()}}
           <h4>Online Friend <span class="badge badge-secondary">{{this.friends ? this.friends.length : 0 }}</span> 
-          <button type="button" class="btn btn-default btn-info" aria-label="Left Align" @click="friends=null">
+          <button type="button" class="btn btn-default btn-info" aria-label="Left Align" @click="refreshFriendList()">
             <span class="glyphicon glyphicon-refresh" aria-hidden="true">refresh</span>
           </button></h4>
-          <ol id="friendlist" style="list-style-type: none;height:400px; width:500px; overflow-y: auto;">
-            <li v-for="friend in friends">
-              <div class="media">
-                <img class="mr-3 border border-dark" :src=friend.currentAvatarImageUrl style='width:120px; height:80px'>
-                <div class="media-body">
-                  <h5 class="mt-0">{{friend.displayName}}</h5>
-                  In world : {{friend.worldName}}
+          <!-- <ol id="friendlist" style="list-style-type: none;height:400px; width:500px; overflow-y: auto;"> -->
+            <transition-group name="list" tag="ul" class="list" >
+              <li v-for="(friend,i) in friends" v-bind:key="i">
+                <div class="media">
+                  <img class="mr-3 border border-dark" :src=friend.currentAvatarImageUrl style='width:120px; height:80px'>
+                  <div class="media-body">
+                    <h5 class="mt-0">{{friend.displayName}}</h5>
+                    In world : {{friend.worldName}}
+                  </div>
                 </div>
-              </div>
-            </li>
-          </ol>
+              </li>
+            </transition-group>
+          <!-- </ol> -->
         </div>
       </div>
     </div>
@@ -79,6 +81,11 @@
       logout () {
         this.$store.dispatch('setAPI', {api: null})
         this.$router.push({name: 'login-page'})
+      },
+      refreshFriendList () {
+        this.api.api.getFriend().then(friends => {
+          this.friends = friends
+        })
       }
     }
   }
@@ -105,6 +112,13 @@
     height: 100vh;
     padding: 60px 80px;
     width: 100vw;
+  }
+
+  .list {
+    list-style-type: none;
+    height:400px;
+    width:500px;
+    overflow-y: auto;
   }
 
   #logo {
@@ -170,5 +184,19 @@
   .doc button.alt {
     color: #42b983;
     background-color: transparent;
+  }
+
+  /* Enter and leave animations can use different */
+  /* durations and timing functions.              */
+  .list-enter-active {
+    transition: all .3s ease;
+  }
+  .list-leave-active {
+    transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+  .list-enter, .list-leave-to
+  /* .list-leave-active below version 2.1.8 */ {
+    transform: translateX(10px);
+    opacity: 0;
   }
 </style>
